5caefa12875ccaabe74da1eea79b1cd6d842cf98
Pierre-Emile Ferron <peferron@gmail.com>
2015-06-22 02:35:33 -0700

    Update dist

--- dist/playbyplay.js
+++ dist/playbyplay.js
@@ -2,58 +2,74 @@
     typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) : typeof define === 'function' && define.amd ? define(['exports'], factory) : factory(global.playbyplay = {});
 })(this, function (exports) {
 
-    var entities = {
-        '&': '&amp;',
-        '<': '&lt;',
-        '>': '&gt;',
-        '"': '&quot;',
-        '\'': '&#39;',
-        '/': '&#x2F;'
+    var escapeClass = function escapeClass(str) {
+        return String(str).replace(/[^a-z0-9]/g, '');
     };
 
     var escapeHTML = function escapeHTML(str) {
-        return String(str).replace(/[&<>"'\/]/g, function (s) {
-            return entities[s];
-        });
+        var div = document.createElement('div');
+        div.appendChild(document.createTextNode(str));
+        return div.innerHTML;
     };
 
     var containerInnerHTML = function containerInnerHTML(runs) {
-        return '<button class="playbyplay__hide">&lt; Back</button>' + ('' + (runs.length ? runsHTML(runs) : 'History is empty.'));
+        return '<button class="playbyplay-button playbyplay-hide">&lt; Back</button>' + ('' + (runs.length ? runsHTML(runs) : emptyHTML));
     };
 
     var runsHTML = function runsHTML(runs) {
-        return '<button class="playbyplay__clear">Clear</button>\n<table class="playbyplay__runs">' + runs.map(runHTML).join('') + '</table>';
+        return '<button class="playbyplay-button playbyplay-clear">Clear</button>\n<table class="playbyplay-runs">\n    ' + runs.map(runHTML).join('') + '\n</table>';
     };
 
     var runHTML = function runHTML(run, index) {
-        return '<tr class="playbyplay__run ' + statusClass(run) + '">\n    <td class="playbyplay__run__col playbyplay__run__input">\n        <pre><code>' + escapeHTML(run.input) + '</code></pre>\n    </td>\n    <td class="playbyplay__run__col playbyplay__run__output">\n        <pre>' + escapeHTML(run.output) + '</pre>\n    </td>\n    <td class="playbyplay__run__col">\n        <button class="playbyplay__run__restore" data-index="' + escapeHTML(index) + '">Restore</button>\n    </td>\n</tr>';
+        return '<tr class="playbyplay-run ' + statusClass(run) + '">\n    <td class="playbyplay-col playbyplay-input">\n        <pre class="playbyplay-pre"><code>' + escapeHTML(run.input) + '</code></pre>\n    </td>\n    <td class="playbyplay-col playbyplay-output">\n        <pre class="playbyplay-pre">' + escapeHTML(run.output) + '</pre>\n    </td>\n    <td class="playbyplay-col">\n        <button class="playbyplay-button playbyplay-restore" data-index="' + index + '">\n            Restore\n        </button>\n    </td>\n</tr>';
     };
 
     var statusClass = function statusClass(run) {
-        return run.status ? 'playbyplay__run--' + escapeHTML(run.status) : '';
+        return run.status ? 'playbyplay-status-' + escapeClass(run.status) : '';
     };
+
+    var emptyHTML = '<span class="playbyplay-empty">History is empty.</span>';
 
     var key = 'playbyplay_0fE#n9asNy4^MD1jfj&!';
     var containerId = 'playbyplay';
 
-    function save(run, options, callback) {
+    var supported = localhistory.supported;
+
+    function append(run, options, callback) {
         setTimeout(function () {
-            localhistory.save(key, run, options, callback);
+            localhistory.append(key, run, options, callback);
         }, 0);
     }
 
-    function show(callback) {
+    // options is optional, callback is required. Usually, required arguments go first, but having the
+    // callback last leads to cleaner calling code with closures.
+    function show(options, callback) {
+        var cb = typeof options === 'function' ? options : callback;
+        var opts = fillShowOptions(typeof options === 'object' ? options : {});
+
         localhistory.load(key, function (err, runs) {
             if (err) {
-                callback(err);
+                if (opts.onShow) {
+                    opts.onShow(err);
+                }
                 return;
             }
-            showRuns(runs, callback);
+            showRuns(runs, opts, cb);
         });
     }
 
-    function showRuns(runs, callback) {
+    function fillShowOptions(options) {
+        if (!options.parent) {
+            options.parent = document.body;
+        }
+        return options;
+    }
+
+    function showRuns(runs, options, callback) {
         hide();
+
+        // The most recently appended runs should be on top.
+        runs.reverse();
 
         var container = document.createElement('div');
 
@@ -62,17 +78,19 @@
 
         container.addEventListener('click', function (e) {
             switch (e && e.target && e.target.className) {
-                case 'playbyplay__hide':
+                case 'playbyplay-button playbyplay-hide':
                     hide();
                     callback(null);
                     return;
-                case 'playbyplay__clear':
+
+                case 'playbyplay-button playbyplay-clear':
                     hide();
-                    localhistory.clear(key, function () {
+                    clear(function () {
                         show(callback);
                     });
                     return;
-                case 'playbyplay__run__restore':
+
+                case 'playbyplay-button playbyplay-restore':
                     // Use getAttribute instead of dataset for compatibility:
                     // http://caniuse.com/#feat=dataset
                     var index = e.target.getAttribute('data-index');
@@ -82,7 +100,16 @@
             }
         }, false);
 
-        document.body.appendChild(container);
+        var err = null;
+        try {
+            options.parent.appendChild(container);
+        } catch (e) {
+            err = e;
+        }
+
+        if (options.onShow) {
+            options.onShow(err);
+        }
     }
 
     function hide() {
@@ -92,8 +119,13 @@
         } catch (e) {}
     }
 
-    exports.save = save;
+    function clear(callback) {
+        localhistory.clear(key, callback);
+    }
+
+    exports.supported = supported;
+    exports.append = append;
     exports.show = show;
+    exports.clear = clear;
 });
 //# sourceMappingURL=./playbyplay.js.map
-// eslint-disable-line no-empty
